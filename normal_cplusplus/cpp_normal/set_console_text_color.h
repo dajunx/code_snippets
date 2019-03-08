9cf0f2baff4adf737509da7031bfbd509569dfae
lin_work_win <892888763@qq.com>
2019-03-08 14:43:20 +0800

    在控制台程序中打印内容时，自定义颜色和背景

---
+++ normal_cplusplus/cpp_normal/set_console_text_color.h
@@ -0,0 +1,20 @@
+﻿
+/**c++ console 控制台设置打印文本内容颜色和背景颜色
+ * 来源：https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
+ */
+#include "cplusplus_common_def.hpp"
+// header file.
+// ##include <windows.h>
+
+bool test_print_color_text_in_console() {
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  // you can loop k higher to see more color choices
+  for (int k = 1; k < 255; k++)
+  {
+    // pick the colorattribute k you want
+    SetConsoleTextAttribute(hConsole, k);
+    cout << k << " I want to be nice today!" << endl;
+  }
+
+  return true;
+}
---
+++ normal_cplusplus/cpp_normal/string_strtok.h
@@ -0,0 +1,16 @@
+﻿#include "cplusplus_common_def.hpp"
+//数组拷贝
+
+bool test_c_string_strtok() {
+  char str[] ="- This, a sample string.";
+  char * pch;
+  printf ("Splitting string \"%s\" into tokens:\n",str);
+  pch = strtok (str," ,.-");
+  while (pch != NULL)
+  {
+    printf ("%s\n",pch);
+    pch = strtok (NULL, " ,.-");
+  }
+
+  return true;
+}
