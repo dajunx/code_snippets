611dccb83f5f3bf2ae9d493dd6b2540c77cff09f
lin_work_win <892888763@qq.com>
2019-03-21 14:54:28 +0800

    refactor(network.linux.epoll):挑战代码逻辑和变量命名，增加可读性

--- network_linux/network/epoll/net_epoll_server.h
+++ network_linux/network/epoll/net_epoll_server.h
@@ -16,24 +16,17 @@
 #define MAXLINE 10240
 
 int handle_net_epoll_server(int connfd);
-int setnonblocking(int sockfd) {
-  if (fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFD, 0) | O_NONBLOCK) == -1) {
-    return -1;
-  }
-
-  return 0;
-}
 
 bool test_net_epoll_server() {
   int servPort = 88960;
   int listenq = 1024;
-  int listenfd, connfd, epoll_create_fd, current_fd_num, acceptCount = 0;
+  int listenfd, connfd, epoll_fd, current_fd_num = 1, acceptCount = 0;
   struct sockaddr_in servaddr, cliaddr;
   socklen_t socklen = sizeof(struct sockaddr_in);
   struct epoll_event ev;
-  struct epoll_event events[MAXEPOLLSIZE];
+  struct epoll_event happen_events[MAXEPOLLSIZE];
 
-  /* 设置每个进程允许打开的最大文件数 */
+  // 设置每个进程允许打开的最大文件数
   struct rlimit rt;
   rt.rlim_max = rt.rlim_cur = MAXEPOLLSIZE;
   if (setrlimit(RLIMIT_NOFILE, &rt) == -1) {
@@ -56,11 +49,6 @@ bool test_net_epoll_server() {
   int opt = 1;
   if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
     perror("setsockopt error");
-    exit(0);
-  }
-
-  if (setnonblocking(listenfd) < 0) {
-    perror("setnonblock error");
   }
 
   if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(struct sockaddr)) ==
@@ -74,35 +62,38 @@ bool test_net_epoll_server() {
     return -1;
   }
 
-  /* 创建 epoll 句柄，把监听 socket 加入到 epoll 集合里 */
-  epoll_create_fd = epoll_create(MAXEPOLLSIZE);
+  // 1.创建 epoll 句柄，把监听 socket 加入到 epoll 集合里
+  epoll_fd = epoll_create(MAXEPOLLSIZE);
   ev.events = EPOLLIN | EPOLLET;
   ev.data.fd = listenfd;
 
-  if (epoll_ctl(epoll_create_fd, EPOLL_CTL_ADD, listenfd, &ev) < 0) {
+  if (fcntl(listenfd, F_SETFL, fcntl(listenfd, F_GETFD, 0) | O_NONBLOCK) == -1) {
+    perror("fcntl error");
+  }
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listenfd, &ev) < 0) { // 2.事件注册
     fprintf(stderr, "epoll set insertion error: fd=%d\n", listenfd);
     return -1;
   }
 
-  current_fd_num = 1;
   printf("epollserver startup,port %d, max connection is %d, backlog is %d\n",
          servPort, MAXEPOLLSIZE, listenq);
 
+  /** 
+   * 等待有事件发生：1.新连入的客户端； 2.已连入客户端请求的数据；
+   */
   for (;;) {
-    /* 等待有事件发生 */
+    int happen_fd_num = -1; // 存放I/O上已准备好的文件描述符数目;
+    happen_fd_num = epoll_wait(epoll_fd, happen_events, current_fd_num, -1); // 3.收集在epoll监控的事件中已经发生的事件
 
-    int ready_fd_num = -1; // 存放I/O上已准备好的文件描述符数目;
-    ready_fd_num = epoll_wait(epoll_create_fd, events, current_fd_num, -1);
-
-    if (ready_fd_num == -1) {
+    if (happen_fd_num == -1) {
       perror("epoll_wait");
       continue;
     }
 
-    /* 处理所有事件 */
-    for (int n = 0; n < ready_fd_num; ++n) {
+    /* 处理所有发生的事件 */
+    for (int n = 0; n < happen_fd_num; ++n) {
       // 新客户端连入
-      if (events[n].data.fd == listenfd) {
+      if (happen_events[n].data.fd == listenfd) {
         connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &socklen);
 
         if (connfd < 0) {
@@ -121,14 +112,14 @@ bool test_net_epoll_server() {
           continue;
         }
 
-        if (setnonblocking(connfd) < 0) {
-          perror("setnonblocking error");
-        }
-
         ev.events = EPOLLIN | EPOLLET;
         ev.data.fd = connfd;
 
-        if (epoll_ctl(epoll_create_fd, EPOLL_CTL_ADD, connfd, &ev) < 0) {
+        if (fcntl(connfd, F_SETFL, fcntl(connfd, F_GETFD, 0) | O_NONBLOCK) == -1) {
+          perror("fcntl error");
+        }
+        // 2.把连入的客户端fd加入 epoll监听事件列表中，用来监听是否有数据到来;
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connfd, &ev) < 0) {
           fprintf(stderr, "add socket '%d' to epoll failed: %s\n", connfd,
                   strerror(errno));
           return -1;
@@ -136,13 +127,14 @@ bool test_net_epoll_server() {
 
         current_fd_num++;
         continue;
+      } else { // 不是新客户端连入，就是已连入客户端数据传输
+        // 处理客户端请求
+        if (handle_net_epoll_server(happen_events[n].data.fd) < 0) {
+          epoll_ctl(epoll_fd, EPOLL_CTL_DEL, happen_events[n].data.fd, &ev);
+          current_fd_num--;
+        }
       }
 
-      // 处理客户端请求
-      if (handle_net_epoll_server(events[n].data.fd) < 0) {
-        epoll_ctl(epoll_create_fd, EPOLL_CTL_DEL, events[n].data.fd, &ev);
-        current_fd_num--;
-      }
     }
   }
 
